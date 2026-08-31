f648ed51f863db6f87d9c9f9418ca152a7d46c15
protom <office@protom.eu>
2026-08-31 17:28:25 +0200

    btcpayserver: package version 2.4.3

---
+++ pkgs/btcpayserver/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  buildDotnetModule,
+  fetchFromGitHub,
+  dotnetCorePackages,
+  altcoinSupport ? false,
+}:
+
+buildDotnetModule rec {
+  pname = "btcpayserver";
+  version = "2.4.3";
+
+  src = fetchFromGitHub {
+    owner = "btcpayserver";
+    repo = "btcpayserver";
+    tag = "v${version}";
+    hash = "sha256-4rDCvC0YmU9ftCcv2QbVDW4e4a/jdMDDJLDB32vQid4=";
+  };
+
+  projectFile = "BTCPayServer/BTCPayServer.csproj";
+  nugetDeps = ./deps.json;
+
+  dotnet-sdk = dotnetCorePackages.sdk_10_0;
+  dotnet-runtime = dotnetCorePackages.aspnetcore_10_0;
+
+  buildType = if altcoinSupport then "Altcoins-Release" else "Release";
+
+  # macOS has a case-insensitive filesystem, so these two can be the same file
+  postFixup = ''
+    mv $out/bin/{BTCPayServer,btcpayserver} || :
+  '';
+
+  meta = {
+    description = "Self-hosted, open-source cryptocurrency payment processor";
+    homepage = "https://btcpayserver.org";
+    changelog = "https://github.com/btcpayserver/btcpayserver/blob/v${version}/Changelog.md";
+    maintainers = with lib.maintainers; [
+      kcalvinalvin
+      erikarvstedt
+    ];
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+}
